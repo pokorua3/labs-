2b5e2b260a39da458e021feff8e8b03a74262070
pokorua3 <pokorua3@gmail.com>
2025-02-16 13:43:34 +0300

    Add files via upload

---
+++ lab_1.py
@@ -0,0 +1,13 @@
+import random
+#random.seed(116)
+#print(random.randint(1, 15))
+#14 задача
+
+numbers = set(map(str, range(1, 11)))
+summ = 0
+file = open('C:/Users/dasha/OneDrive/Рабочий стол/lab1.txt', "r")
+for i in file.read().split():
+    chifr = i.strip(",. ")
+    if chifr in numbers:
+        summ += int(chifr)
+print(f'Сумма чисел в тексте: {summ}')
---
+++ lab_2.py
@@ -0,0 +1,29 @@
+import random
+#random.seed(116)
+#print(random.randint(1, 20))
+#20 задача
+
+file = open('C:/Users/dasha/OneDrive/Рабочий стол/lab2.txt', 'r')
+numbers = list(map(int, file.readline().split()))
+file.close()
+first_otr = -1
+min_pol = -1
+min_positive = float('inf')
+for i in range(len(numbers)):
+    if numbers[i] < 0 and first_otr == -1:
+        first_otr = i
+    if numbers[i] > 0 and numbers[i] < min_positive:
+        min_positive = numbers[i]
+        min_pol = i
+
+if first_otr != -1 and min_pol != -1:
+    if first_otr < min_pol:
+        result_sum = sum(numbers[i] for i in range(0, len(numbers), 2))
+    else:
+        result_sum = sum(numbers[i] for i in range(1, len(numbers), 2))
+else:
+    result_sum = 0
+
+file = open("C:/Users/dasha/OneDrive/Рабочий стол/lab2_result.txt", "w")
+file.write(str(result_sum))
+file.close()
