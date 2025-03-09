999c432984bfed5bd5f6915ea44e3dc90b42c680
pokorua3 <pokorua3@gmail.com>
2025-03-09 20:39:59 +0300

    Update lab_1.py

--- lab_1.py
+++ lab_1.py
@@ -4,11 +4,11 @@ import random
 #14 задача
 #Текст содержит слова и целые числа от 1 до 10. Найти сумму включенных в текст чисел
 
-text = 
+text = 'У меня есть текст, в котором содержатся и цифры 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 и буквы. Ещё есть числа 13 , 12  , 45 .'
 numbers = set(map(str, range(1, 11)))
 summ = 0
 file = open('lab1.txt', "r")
-for i in file.read().split():
+for i in text.split():
     chifr = i.strip(",. ")
     if chifr in numbers:
         summ += int(chifr)
