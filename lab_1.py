6aeaf722b86d04d424ca4823aa25ee47aa9874b2
pokorua3 <pokorua3@gmail.com>
2025-03-09 20:41:06 +0300

    Update lab_1.py

--- lab_1.py
+++ lab_1.py
@@ -7,7 +7,6 @@ import random
 text = 'У меня есть текст, в котором содержатся и цифры 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 и буквы. Ещё есть числа 13 , 12  , 45 .'
 numbers = set(map(str, range(1, 11)))
 summ = 0
-file = open('lab1.txt', "r")
 for i in text.split():
     chifr = i.strip(",. ")
     if chifr in numbers:
